5b40dc7fbbd0f5de093916f4d1a59a73f35e47d2
kirill <holomeeev2011@gmail.com>
2025-11-09 23:44:44 +0300

    Пример

---
+++ ClassExample/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Class
+{
+    class Man
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+
+    class Employee : Man
+    {
+        public decimal Salary { get; set; }
+        public string Title { get; set; }
+    }
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {}
+    }
+}
