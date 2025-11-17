8e4981dfc80e26cfaf0a92e65cee91fe9f8b0ed8
kirill <holomeeev2011@gmail.com>
2025-11-17 23:57:52 +0300

    абстракт

--- AbstractClasses/Program.cs
+++ AbstractClasses/Program.cs
@@ -2,13 +2,36 @@
 {
     abstract class Transport
     {
+        public void Move()
+        {
+            Console.WriteLine("Транспортное средство движется!");
+        }
+    }
+
+    class Car : Transport
+    {
+        
+    }
+
+    class Aircraft : Transport
+    {
+        
+    }
+    class Ship : Transport
+    {
         
     }
     static class Program
     {
         static void Main(string[] args)
         {
-            
+            Transport car = new Car();
+            Transport plane = new Aircraft();
+            Transport ship = new Ship();
+
+            car.Move();
+            plane.Move();
+            ship.Move();
         }
     }
 }
